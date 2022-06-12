1b250928354829833630c0f53848348ee9ccff46
Lin Xiang <59595349+RyanLinXiang@users.noreply.github.com>
2022-06-12 22:26:36 +0200

    fixed cors and linting

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
-import express from "express"
-import { router as apiRouter } from "./api/router.js"
-import { createRequire } from "module"
-const require = createRequire(import.meta.url)
+import express from "express";
+import { router as apiRouter } from "./api/router.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-const { NODE_ENV: environment } = process.env
+const { NODE_ENV: environment } = process.env;
 
-const { routes, server: { port } } = require(`./config/${environment || "development"}.json`)
+const {
+  routes,
+  server: { port }
+} = require(`./config/${environment || "development"}.json`);
 
-const app = express()
+const app = express();
 
-app.use(routes.api, apiRouter)
+app.use(function (_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
-app.listen(port, () => console.log(`Server has startet and is listening on port ${port}`))
+app.use(routes.api, apiRouter);
+
+app.listen(port, () =>
+  console.log(`Server has startet and is listening on port ${port}`)
+);
